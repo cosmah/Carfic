fc314460a265af9f2cdd9031dc916f2fee3a135f
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-10-05 15:43:42 +0300

    working slideshow

--- carfic/src/app/garage/garage.component.ts
+++ carfic/src/app/garage/garage.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-garage',
   templateUrl: './garage.component.html',
   styleUrls: ['./garage.component.css']
 })
-export class GarageComponent {
+export class GarageComponent implements OnInit {
+  images: string[] = [
+    'assets/1.jpeg',
+    'assets/2.jpg',
+    'assets/3.jpg',
+    // Add more image paths as needed
+  ];
 
+  currentIndex: number = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000); // Change image every 3 seconds (adjust as needed)
+  }
 }
