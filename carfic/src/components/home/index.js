e573b6f8ad4abf433c69e31e7492f573f2f1b249
cosmah <cosmahke4@gmail.com>
2024-03-23 21:36:31 +0300

    setting up components

---
+++ carfic/src/components/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+ return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple React component for the home page.</p>
+    </div>
+ );
+};
+
+export default Home;
