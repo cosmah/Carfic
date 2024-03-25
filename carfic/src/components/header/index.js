64197c823c89d21679e74023f97a9497f84aa53f
cosmah <cosmahke4@gmail.com>
2024-03-25 14:54:55 +0300

    setting up slider

--- carfic/src/components/header/index.js
+++ carfic/src/components/header/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
+import cranks from '../../assets/image/cranks.png';
+import lubs from '../../assets/image/lubs.png';
+import mec from '../../assets/image/mec.png';
+import spares1 from '../../assets/image/spares1.png';
 
 const Header = () => {
- return (
+  const images = [cranks, lubs, mec, spares1];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
+
+  const previousImage = () => {
+    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+  };
+
+  return (
     <div>
-      <h1>Welcome to the header page</h1>
-      <p>This is a simple React component for the header page.</p>
+      
+      <div>
+        <button onClick={previousImage}>Previous</button>
+        <img src={images[currentImageIndex]} alt="carousel" onError={(e)=>{e.target.onerror = null; e.target.src="placeholder.jpg"}} />
+        <button onClick={nextImage}>Next</button>
+      </div>
     </div>
- );
+  );
 };
 
 export default Header;
--- carfic/src/components/home/index.js
+++ carfic/src/components/home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../header';
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <h1>Welcome to the Home Page</h1>
       <p>This is a simple React component for the home page.</p>
     </div>
