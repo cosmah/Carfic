e8fbd570fbc9924ec6fb19a1e16fe0ab3ac4fb34
cosmah <cosmahke4@gmail.com>
2024-03-26 21:01:30 +0300

    working on the footer style

--- carfic/src/components/footer/index.js
+++ carfic/src/components/footer/index.js
@@ -1,41 +1,57 @@
 // index.js
 import React from 'react';
 import './index.css';
+import logo from '../../assets/image/services-6.png'; // Correcting the path
 
 const Footer = () => {
  return (
     <footer className="footer">
       <div className="footer-content">
-        <div className='logo'>
-          <h4>Carfic </h4>
+        {/* Logo Column */}
+        <div className="footer-column logo-column">
+          <div className='logo'>
+          <img src={logo} alt="Carfic Logo" />
+          </div>
         </div>
         
-        <div className="footer-section links">
-          <ul>
-            <a href="#"><li>Service Center</li></a>
-            <a href="#"><li>Auto Parts</li></a>
-            <a href="#"><li>About us</li></a>
-            <a href="#"><li>Appointment</li></a>
-            <a href="#"><li>Blog</li></a>
-            <a href="#"><li>Contact</li></a>
-          </ul>
-        </div>
-        <div className="talk-section links">
-        <p>Trust us for a thorough and reliable automotive experience</p>
+        {/* Links Column */}
+        <div className="footer-column links-column">
+          <div className="footer-section links">
+            <h5>Links</h5>
+            <ul>
+              <li><a href="#">Service Center</a></li>
+              <li><a href="#">Auto Parts</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Appointment</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
         </div>
 
-        <div className="contact-section links">
-          
-          <div className="contact">
-            <span><i className="fas fa-phone"></i> &nbsp; +256 20 546 5930</span>
-            <span><i className="fas fa-phone"></i> &nbsp; +256 39 324 8791</span>
-            <span><i className="fas fa-envelope"></i> &nbsp; care@axlefoley.com</span>
-            <span><i className="fas fa-map-marker-alt"></i> &nbsp; Plot 586, Kira - Kasangati Road</span>
+        {/* Mission Column */}
+        <div className="footer-column mission-column">
+          <div className="footer-section mission">
+            <h5>Mission</h5>
+            <p>Trust us for a thorough and reliable automotive experience</p>
           </div>
-          <div className="hours">
-            <p>Monday - Friday: 08:00 - 17:00</p>
-            <p>Saturday: 08:00 - 13:00</p>
-            <p>Sundays & Public Holidays Closed</p>
+        </div>
+
+        {/* Contact Column */}
+        <div className="footer-column contact-column">
+          <div className="footer-section contact-section">
+            <h5>Contact</h5>
+            <div className="contact">
+              <span><i className="fas fa-phone"></i> &nbsp; +256 20 546 5930</span>
+              <span><i className="fas fa-phone"></i> &nbsp; +256 39 324 8791</span>
+              <span><i className="fas fa-envelope"></i> &nbsp; care@axlefoley.com</span>
+              <span><i className="fas fa-map-marker-alt"></i> &nbsp; Plot 586, Kira - Kasangati Road</span>
+            </div>
+            <div className="hours">
+              <p>Monday - Friday: 08:00 - 17:00</p>
+              <p>Saturday: 08:00 - 13:00</p>
+              <p>Sundays & Public Holidays Closed</p>
+            </div>
           </div>
         </div>
       </div>
