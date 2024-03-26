dcafb3824a76d867b84c92d1f4d147f494c65971
cosmah <cosmahke4@gmail.com>
2024-03-26 14:02:06 +0300

    working on banner

--- carfic/src/components/header/index.js
+++ carfic/src/components/header/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// index.js
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import cranks from '../../assets/image/cranks.png';
 import lubs from '../../assets/image/lubs.png';
@@ -6,27 +7,33 @@ import mec from '../../assets/image/mec.png';
 import spares1 from '../../assets/image/spares1.png';
 
 const Header = () => {
-  const images = [cranks, lubs, mec, spares1];
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+ const images = [cranks, lubs, mec, spares1];
+ const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const nextImage = () => {
-    setCurrentImageIndex((currentImageIndex + 1) % images.length);
-  };
+ useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    }, 5000); // Change image every 5 seconds
 
-  const previousImage = () => {
-    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
-  };
+    return () => clearInterval(timer); // Clean up on component unmount
+ }, [currentImageIndex, images.length]);
 
-  return (
-    <div>
-      
-      <div>
-        <button onClick={previousImage}>Previous</button>
-        <img src={images[currentImageIndex]} alt="carousel" onError={(e)=>{e.target.onerror = null; e.target.src="placeholder.jpg"}} />
-        <button onClick={nextImage}>Next</button>
-      </div>
+ return (
+    <div className="banner">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt="carousel"
+          className={index === currentImageIndex ? 'active' : ''}
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = "placeholder.jpg";
+          }}
+        />
+      ))}
     </div>
-  );
+ );
 };
 
 export default Header;
