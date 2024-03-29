e690a9f2177b7aa58a4c525b60560bd07ad9fbaf
cosmah <cosmahke4@gmail.com>
2024-03-29 14:01:19 +0300

    impleenting the cards in the home component

--- carfic/src/components/home/index.js
+++ carfic/src/components/home/index.js
@@ -1,16 +1,65 @@
 import React from 'react';
 import Header from '../header';
 import Index from '../footer';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from 'react-bootstrap';
+import garage from '../../assets/image/garage.jpg';
+import lubs from '../../assets/image/lubs.jpg';
+import spares from '../../assets/image/auto03pix.jpg';
 
 const Home = () => {
-  return (
+ return (
     <div>
       <Header />
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple React component for the home page.</p>
+      <div className="container">
+        <div className="row justify-content-around">
+          {/* Column 1 */}
+          <div className="col-12 col-md-4">
+            <Card className="card">
+              <Card.Img variant="top" src={garage} className="card-img-top" />
+              <Card.Body className="card-body">
+                <Card.Title className="card-title">Card Title 1</Card.Title>
+                <Card.Text className="card-text">
+                 Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary" className="card-button">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Column 2 */}
+          <div className="col-12 col-md-4">
+            <Card className="card">
+            <Card.Img variant="top" src={lubs} className="card-img-top" />
+              <Card.Body className="card-body">
+                <Card.Title className="card-title">Card Title 2</Card.Title>
+                <Card.Text className="card-text">
+                 Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary" className="card-button">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Column 3 */}
+          <div className="col-12 col-md-4">
+            <Card className="card">
+            <Card.Img variant="top" src={spares} className="card-img-top" />
+              <Card.Body className="card-body">
+                <Card.Title className="card-title">Card Title 3</Card.Title>
+                <Card.Text className="card-text">
+                 Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary" className="card-button">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
       <Index />
     </div>
-  );
+ );
 };
 
 export default Home;
