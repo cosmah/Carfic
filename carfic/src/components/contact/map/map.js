77a486326d6025ce7594cad34a5a9fa745608d3a
cosmah <cosmahke4@gmail.com>
2024-04-15 15:40:17 +0300

    done with the map

--- carfic/src/components/contact/map/map.js
+++ carfic/src/components/contact/map/map.js
@@ -4,6 +4,7 @@ import { Loader } from '@googlemaps/js-api-loader';
 
 const Locator = ({ apiKey }) => {
  const [map, setMap] = useState(null);
+ const [infoWindow, setInfoWindow] = useState(null);
 
  useEffect(() => {
     const loadGoogleMaps = async () => {
@@ -15,10 +16,33 @@ const Locator = ({ apiKey }) => {
 
         await loader.load();
         const mapInstance = new google.maps.Map(document.getElementById('map'), {
-          center: { lat: -34.397, lng: 150.644 },
-          zoom: 8,
+          center: { lat: 0.3461648898330333, lng: 32.646774982820716 },
+          zoom: 15,
         });
+
+        // Create a marker at the specified location
+        const marker = new google.maps.Marker({
+          position: { lat: 0.3461648898330333, lng: 32.646774982820716 },
+          map: mapInstance,
+          title: "Carfic Automotive Services and spare parts",
+        });
+
+        // Create an InfoWindow
+        const infoWindowInstance = new google.maps.InfoWindow({
+          content: '<div><h2>Carfic Automotive Services and spare parts</h2><p>Kireka, opposite Kireka police station, Kampala, Uganda</p></div>',
+        });
+
+        // Add a click event listener to the marker to open the InfoWindow
+        marker.addListener('click', () => {
+          infoWindowInstance.open({
+            anchor: marker,
+            map: mapInstance,
+            shouldFocus: false,
+          });
+        });
+
         setMap(mapInstance);
+        setInfoWindow(infoWindowInstance);
       } catch (error) {
         console.error('Error loading Google Maps API:', error);
       }
