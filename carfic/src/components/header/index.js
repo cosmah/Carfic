27788909c6ed7c4b0eb036256a5a63de3b2bd62d
cosmah <cosmahke4@gmail.com>
2024-04-17 18:40:45 +0300

    working on responsiveness

--- carfic/src/components/header/index.js
+++ carfic/src/components/header/index.js
@@ -1,56 +1,53 @@
 // index.js
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import cranks from '../../assets/image/cranks.png';
-import lubs from '../../assets/image/lubs.png';
-import mec from '../../assets/image/mec.png';
-import spares1 from '../../assets/image/spares1.png';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import cranks from "../../assets/image/cranks.png";
+import lubs from "../../assets/image/lubs.png";
+import mec from "../../assets/image/mec.png";
+import spares1 from "../../assets/image/spares1.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
- const images = [cranks, lubs, mec, spares1];
- const slogans = [
-     "Experience the best automotive services",
-     "Your trusted partner for automotive needs",
-     "Quality and reliability in every service",
-     "Driving your dreams forward",
- ];
- const [currentImageIndex, setCurrentImageIndex] = useState(0);
- const [currentSloganIndex, setCurrentSloganIndex] = useState(0);
- 
- useEffect(() => {
-     const timer = setInterval(() => {
-       setCurrentImageIndex((currentImageIndex + 1) % images.length);
-       setCurrentSloganIndex((currentSloganIndex + 1) % slogans.length);
-     }, 5000);
-     
-     return () => clearInterval(timer);
- }, [currentImageIndex, currentSloganIndex, images.length, slogans.length]);
- 
- return (
+  const images = [cranks, lubs, mec, spares1];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, [currentImageIndex, images.length]);
+
+  return (
     <div className="banner">
       {images.map((image, index) => (
         <img
           key={index}
           src={image}
           alt="carousel"
-          className={index === currentImageIndex ? 'active' : ''}
+          className={index === currentImageIndex ? "active" : ""}
           onError={(e) => {
             e.target.onerror = null;
             e.target.src = "placeholder.jpg";
           }}
         />
       ))}
-      <div className="slogan-container">
-        {slogans.map((slogan, index) => (
-          <h2 key={index} style={index === slogans.length - 1 ? {fontWeight: 'bold'} : {}}>{slogan}</h2>
-        ))}
+      <div className="slogans">
+        <div className="paragraph">
+          <p>Experience the best automotive services</p>
+          <p>Your trusted partner for automotive needs</p>
+          <p>Quality and reliability in every service</p>
+          <p className="emphasis">Driving your dreams forward</p>
+        </div>
+
         <Link to="/services">
-          <button className="cta-button">BOOK A SERVICE</button>
+          <button>Book a Service</button>
         </Link>
       </div>
     </div>
- );
-}
+  );
+};
 
 export default Header;
--- carfic/src/components/home/cards/card.js
+++ carfic/src/components/home/cards/card.js
@@ -11,73 +11,76 @@ import lubs from "../../../assets/image/lubs.jpg";
 import spares from "../../../assets/image/auto03pix.jpg";
 import { styled } from "@mui/system"; // Import styled from @mui/system
 import "./card.css"; // Import the CSS file
+import { Link } from 'react-router-dom';
+
 
 // Create a styled Button component
 const LargeButton = styled(Button)({
   padding: "10px 20px", // Adjust padding to make the button larger
   fontSize: "1rem", // Adjust font size as needed
 });
-
-const MediaCard = ({ image, title, description }) => (
+const MediaCard = ({ image, title, description, path }) => (
   <Card className="card">
-    <CardMedia
-      className="cardMedia"
-      component="img"
-      height="200"
-      image={image}
-      alt={title}
-    />
-    <CardContent>
-
-<Typography 
-  gutterBottom 
-  variant="h5" 
-  component="div"
-  sx={{ fontSize: '2.8rem' }} // Increase the font size of the title
->
-  {title}
-</Typography>
-
-<Typography
-  variant="body2"
-  color="textSecondary"
-  component="p"
-  className="description"
-  sx={{ fontSize: '1.6rem' }} // Increase the font size of the description
->
-  {description}
-</Typography>
-    </CardContent>
-    <CardActions>
-      <LargeButton size="larger">Learn More</LargeButton>
-    </CardActions>
+     <CardMedia
+       className="cardMedia"
+       component="img"
+       height="200"
+       image={image}
+       alt={title}
+     />
+     <CardContent>
+       <Typography 
+         gutterBottom 
+         variant="h5" 
+         component="div"
+         sx={{ fontSize: '2.8rem' }} // Increase the font size of the title
+       >
+         {title}
+       </Typography>
+       <Typography
+         variant="body2"
+         color="textSecondary"
+         component="p"
+         className="description"
+         sx={{ fontSize: '1.6rem' }} // Increase the font size of the description
+       >
+         {description}
+       </Typography>
+     </CardContent>
+     <CardActions>
+       <Link to={path}>
+         <LargeButton size="larger">Learn More</LargeButton>
+       </Link>
+     </CardActions>
   </Card>
-);
-
-export default function Cards() {
+ );
+ export default function Cards() {
   return (
-    <Grid container spacing={2} justifyContent="center">
-      <Grid item xs={12} sm={6} md={4}>
-        <MediaCard
-          image={garage}
-          title="Garage"
-          description="Our state-of-the-art garage is equipped with the latest diagnostic tools and repair equipment. We specialize in comprehensive car repairs, from routine maintenance to complex engine overhauls."
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <MediaCard
-          image={lubs}
-          title="Lubs"
-          description="At our service center, we offer a wide range of high-quality lubricants and oils for all your vehicle needs. From engine oils to transmission fluids, we carry a variety of products to ensure your engine runs smoothly and efficiently."
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <MediaCard
-          image={spares}
-          title="Spares"
-          description="We carry a vast selection of genuine spare parts and accessories for a wide range of vehicles. Whether you need a replacement part for your car's engine, transmission, or electrical system, we've got you covered."
-        />
-      </Grid>
-    </Grid>
+     <Grid container spacing={2} justifyContent="center">
+       <Grid item xs={12} sm={6} md={4}>
+         <MediaCard
+           image={garage}
+           title="Garage"
+           description="Our state-of-the-art garage is equipped with the latest diagnostic tools and repair equipment. We specialize in comprehensive car repairs, from routine maintenance to complex engine overhauls."
+           path="/services"
+         />
+       </Grid>
+       <Grid item xs={12} sm={6} md={4}>
+         <MediaCard
+           image={lubs}
+           title="Lubs"
+           description="At our service center, we offer a wide range of high-quality lubricants and oils for all your vehicle needs. From engine oils to transmission fluids, we carry a variety of products to ensure your engine runs smoothly and efficiently."
+           path="/services"
+         />
+       </Grid>
+       <Grid item xs={12} sm={6} md={4}>
+         <MediaCard
+           image={spares}
+           title="Spares"
+           description="We carry a vast selection of genuine spare parts and accessories for a wide range of vehicles. Whether you need a replacement part for your car's engine, transmission, or electrical system, we've got you covered."
+           path="/spares"
+         />
+       </Grid>
+     </Grid>
   );
-}
+ }
