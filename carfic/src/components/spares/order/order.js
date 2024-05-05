1c717007c71046e8a5a87e83b51d70cf12a71ff1
cosmah <cosmahke4@gmail.com>
2024-05-05 16:24:34 +0300

    order form

--- carfic/src/components/spares/order/order.js
+++ carfic/src/components/spares/order/order.js
@@ -1,82 +1,137 @@
 import React from "react";
 import "./order.css";
+import { useForm, ValidationError } from '@formspree/react';
 
 const Order = () => {
+
+  const [state, handleSubmit] = useForm("mwkgyayv");
+  if (state.succeeded) {
+    return (
+      <Order />
+    );
+  }
+
   return (
     <div className="order-parts-form">
       <h1 className="form-title">ORDER PARTS</h1>
-      <form className="form-container">
+      <form className="form-container" onSubmit={handleSubmit}>
         <div className="form-column">
           <label htmlFor="firstName">FIRST NAME *</label>
           <input
             type="text"
-            id="firstName"
+            name="firstName"
             required
             className="input-field"
           />
+          <ValidationError 
+        prefix="FirstName" 
+        field="firstName"
+        errors={state.errors}
+      />
 
           <label htmlFor="lastName">LAST NAME *</label>
           <input
             type="text"
-            id="lastName"
+            name="lastName"
             required
             className="input-field"
           />
+          <ValidationError 
+        prefix="LastName" 
+        field="lastName"
+        errors={state.errors}
+      />
 
           <label htmlFor="email">EMAIL</label>
           <input
             type="email"
-            id="email"
+            name="email"
             placeholder="Enter your email"
             className="input-field"
           />
+          <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
 
           <label htmlFor="phone">PHONE NUMBER</label>
           <input
             type="tel"
-            id="phone"
+            name="phone"
             placeholder="Enter your phone number"
             className="input-field"
           />
+          <ValidationError 
+        prefix="Phone" 
+        field="phone"
+        errors={state.errors}
+      />
         </div>
 
         <div className="form-column">
           <label htmlFor="vehicleMakeModel">VEHICLE MAKE / MODEL</label>
           <input
             type="text"
-            id="vehicleMakeModel"
+            name="vehicleMakeModel"
             className="input-field"
           />
+          <ValidationError 
+        prefix="vehicleMakeModel" 
+        field="vehicleMakeModel"
+        errors={state.errors}
+      />
 
           <label htmlFor="vehicleYear">VEHICLE YEAR</label>
           <input
             type="number"
-            id="vehicleYear"
+            name="vehicleYear"
             className="input-field"
           />
+          <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
 
           <label htmlFor="vin">VEHICLE IDENTIFICATION NUMBER</label>
           <input
             type="text"
-            id="vin"
+            name="vin"
             className="input-field"
           />
+          <ValidationError 
+        prefix="Vin" 
+        field="vin"
+        errors={state.errors}
+      />
 
           <label htmlFor="partsNeeded">SPECIFY THE PARTS NEEDED</label>
           <textarea
-            id="partsNeeded"
+            name="partsNeeded"
             placeholder="Specify the parts needed"
             rows="4"
             className="input-field"
           ></textarea>
+          <ValidationError 
+        prefix="partsNeeded" 
+        field="partsNeeded"
+        errors={state.errors}
+      />
         </div>
 
-        <button type="submit" className="submit-button">
-          SUBMIT FORM
-        </button>
+        <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
       </form>
     </div>
   );
 };
+// eslint-disable-next-line
+function App() {
+  return (
+    <Order />
+  );
+}
 
 export default Order;
